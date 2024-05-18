beb4c669460f2ae9c4085d2c18cf859c78c56153
Samitha Perera <73548027+SamithaSandeepa@users.noreply.github.com>
2024-05-19 00:59:35 +0530

    update path

--- trendprediction/views.py
+++ trendprediction/views.py
@@ -1,31 +1,31 @@
+import os
 import json
 import glob
-import random
 import pandas as pd
-import numpy as np
 from joblib import load
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.metrics import accuracy_score
-from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
 from sklearn.multioutput import MultiOutputClassifier
 
-clf_colour = load('model2\clf_colour.hd')
-colour_encoder = load('model2\colour_encoder.hd')
+# Load models and encoders
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+clf_colour = load(os.path.join(BASE_DIR, 'trendprediction', 'model2', 'clf_colour.hd'))
+colour_encoder = load(os.path.join(BASE_DIR, 'trendprediction', 'model2', 'colour_encoder.hd'))
 label_encoders = {
-    'MONTH': load('model2\MONTH_label_encoder.hd'),
-    'Category': load('model2\Category_label_encoder.hd'),
-    'GENDER': load('model2\GENDER_label_encoder.hd'),
-    'Age': load('model2\Age_label_encoder.hd'),
+    'MONTH': load(os.path.join(BASE_DIR, 'trendprediction', 'model2', 'MONTH_label_encoder.hd')),
+    'Category': load(os.path.join(BASE_DIR, 'trendprediction', 'model2', 'Category_label_encoder.hd')),
+    'GENDER': load(os.path.join(BASE_DIR, 'trendprediction', 'model2', 'GENDER_label_encoder.hd')),
+    'Age': load(os.path.join(BASE_DIR, 'trendprediction', 'model2', 'Age_label_encoder.hd')),
 }
-clf_pattern = load('model2\clf_pattern.hd')
-pattern_encoder = load('model2\pattern_encoder.hd')
+clf_pattern = load(os.path.join(BASE_DIR, 'trendprediction', 'model2', 'clf_pattern.hd'))
+pattern_encoder = load(os.path.join(BASE_DIR, 'trendprediction', 'model2', 'pattern_encoder.hd'))
 
 
 class DataStore():
@@ -38,13 +38,12 @@ data = DataStore()
 
 
 def home(request):
-    return render(request, 'website.html')
+    return JsonResponse({'message': 'Welcome to the trend prediction API!'})
 
 
 @csrf_exempt
 def predict_colour(request):
     if request.method == 'POST':
-        # Extract data from the request
         try:
             data = json.loads(request.body)
             year = data.get('year')
@@ -53,11 +52,7 @@ def predict_colour(request):
             gender = data.get('gender')
             age = data.get('age')
         except json.JSONDecodeError:
-            year = request.POST.get('year')
-            month = request.POST.get('month')
-            category = request.POST.get('category')
-            gender = request.POST.get('gender')
-            age = request.POST.get('age')
+            return JsonResponse({'error': 'Invalid JSON'}, status=400)
 
         if None in [year, month, category, gender, age]:
             return JsonResponse({'error': 'Missing parameters'}, status=400)
@@ -85,21 +80,21 @@ def predict_colour(request):
         # Return the prediction
         return JsonResponse({'colour': colour[0]})
     else:
-        # For GET requests, you might want to return instructions or a form for input
         return JsonResponse({'error': 'Please use a POST request with the correct parameters to get a prediction.'})
 
 
 @csrf_exempt
 def predict_pattern(request):
     if request.method == 'POST':
-        # Extract data from the request
-        # data = request.json
-        data = json.loads(request.body)
-        year = data.get('year')
-        month = data.get('month')
-        category = data.get('category')
-        gender = data.get('gender')
-        age = data.get('age')
+        try:
+            data = json.loads(request.body)
+            year = data.get('year')
+            month = data.get('month')
+            category = data.get('category')
+            gender = data.get('gender')
+            age = data.get('age')
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON'}, status=400)
 
         if None in [year, month, category, gender, age]:
             return JsonResponse({'error': 'Missing parameters'}, status=400)
@@ -133,7 +128,8 @@ def predict_pattern(request):
 @csrf_exempt
 def predict_style(request):
     if request.method == 'POST':
-        df = pd.read_csv('csv/model_style.csv')
+        csv_path = os.path.join(BASE_DIR, 'trendprediction', 'csv', 'model_style.csv')
+        df = pd.read_csv(csv_path)
 
         input_features = ['gender', 'season', 'subCategory']
         X = df[input_features]
@@ -152,15 +148,13 @@ def predict_style(request):
 
         model.fit(X_train, y_train)
 
-        # year = request.form.get('year')
-        # month = request.form.get('month')
-        # gender = request.form.get('gender')
-        # category = request.form.get('category')
-
-        data = json.loads(request.body)
-        month = data.get('month')
-        category = data.get('category')
-        gender = data.get('gender')
+        try:
+            data = json.loads(request.body)
+            month = data.get('month')
+            category = data.get('category')
+            gender = data.get('gender')
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON'}, status=400)
 
         input_data = pd.DataFrame({
             'gender': [gender],
@@ -176,8 +170,7 @@ def predict_style(request):
 
 
 def currenttrends(request):
-    if 'tshirts' in request.args:
-        #        Insert code here/ information to retrieve
+    if 'tshirts' in request.GET:
         top1_name = []
         top1_description = []
         top1_score = []
@@ -186,138 +179,73 @@ def currenttrends(request):
         bottom1_score = []
 
         images_path = []
-        imagestshirts = glob.glob('static/img/current_trends/shirt+' + '*.jpeg')
-        for i in range(0, len(imagestshirts)):
-            images_path.append(imagestshirts[i])
-        imagestshirts = glob.glob('static/img/current_trends/shirt-' + '*.jpeg')
-        for i in range(0, len(imagestshirts)):
-            images_path.append(imagestshirts[i])
+        images_folder = os.path.join(BASE_DIR, 'trendprediction', 'static', 'img', 'current_trends')
+        imagestshirts = glob.glob(os.path.join(images_folder, 'shirt+' + '*.jpeg'))
+        for img_path in imagestshirts:
+            images_path.append(img_path)
+        imagestshirts = glob.glob(os.path.join(images_folder, 'shirt-' + '*.jpeg'))
+        for img_path in imagestshirts:
+            images_path.append(img_path)
 
-        colnames = ['sno', 'URL', 'id', 'desc', 'stars', 'num_ratings', 'num_reviews', 'reviews', 'vader_score',
-                    'final_score']
+        colnames = ['sno', 'URL', 'id', 'desc', 'stars', 'num_ratings', 'num_reviews', 'reviews', 'vader_score', 'final_score']
         reqdcolnames = ['id', 'stars', 'desc', 'URL', 'final_score']
-        dataset_csv = pd.read_csv('CurrentTrends/final_csv/tshirts/tshirts_csv_final.csv', names=colnames,
-                                  delimiter=',', on_bad_lines='skip',
-                                  header=None, usecols=reqdcolnames, na_values=" NaN")
+        csv_path = os.path.join(BASE_DIR, 'trendprediction', 'CurrentTrends', 'final_csv', 'tshirts', 'tshirts_csv_final.csv')
+        dataset_csv = pd.read_csv(csv_path, names=colnames, delimiter=',', on_bad_lines='skip', header=None, usecols=reqdcolnames, na_values=" NaN")
         dataset_csv = dataset_csv.dropna()
-        dataset_csv2 = dataset_csv.sort_values(by='final_score', ascending=False)
-        dataset_csv2 = dataset_csv2.reset_index()
+        dataset_csv2 = dataset_csv.sort_values(by='final_score', ascending=False).reset_index()
 
-        # print(dataset_csv2.head())
         for i in range(1, 6):
             top1_name.append(dataset_csv2['desc'][i])
             top1_description.append(dataset_csv2['URL'][i])
             top1_score.append(dataset_csv2['final_score'][i])
-        for i in range((len(dataset_csv2) - 5), len(dataset_csv2)):
+        for i in range(len(dataset_csv2) - 5, len(dataset_csv2)):
             bottom1_name.append(dataset_csv2['desc'][i])
             bottom1_description.append(dataset_csv2['URL'][i])
             bottom1_score.append(dataset_csv2['final_score'][i])
 
-        df = pd.read_csv('CurrentTrends/Leaderboard/tshirt_colour_top_bottom.csv')
-        # print(df.head())
+        df = pd.read_csv(os.path.join(BASE_DIR, 'trendprediction', 'CurrentTrends', 'Leaderboard', 'tshirt_colour_top_bottom.csv'))
         df = df[["Bigram", "Rating", "Count"]]
-
-        # order in the groupby here matters, it determines the json nesting the groupby call makes a pandas series by
-        # grouping 'the_parent' and 'the_child', while summing the numerical column 'child_size'
-        df1 = df.groupby(['Bigram', 'Rating'])['Count'].sum()
-        df1 = df1.reset_index()
-
-        # start a new flare.json document
-        flare = dict()
-        d = {"name": "flare", "children": []}
-        i = 0
+        df1 = df.groupby(['Bigram', 'Rating'])['Count'].sum().reset_index()
+        flare = {'name': 'flare', 'children': []}
+        d = flare
         for line in df1.values:
-            Bigram = line[0]
-            Rating = line[1]
-            Count = line[2]
-            # make a list of keys
-            keys_list = []
-            for item in d['children']:
-                keys_list.append(item['name'])
-
-            # if 'the_parent' is NOT a key in the flare.json yet, append it
+            Bigram, Rating, Count = line
+            keys_list = [item['name'] for item in d['children']]
             if not Bigram in keys_list:
-                d['children'].append({"name": Bigram, "children": [{"name": Rating, "size": Count}]})
-
-            # if 'the_parent' IS a key in the flare.json, add a new child to it
+                d['children'].append({'name': Bigram, 'children': [{'name': Rating, 'size': Count}]})
             else:
-                d['children'][keys_list.index(Bigram)]['children'].append({"name": Rating, "size": Count})
-
-        flare = d
-        e = json.dumps(flare)
-        data.Prod = json.loads(e)
+                d['children'][keys_list.index(Bigram)]['children'].append({'name': Rating, 'size': Count})
+        data.Prod = json.loads(json.dumps(d))
         Prod = data.Prod
 
-        df = pd.read_csv('CurrentTrends/Leaderboard/tshirt_neck_top_bottom.csv')
-        # print(df.head())
+        df = pd.read_csv(os.path.join(BASE_DIR, 'trendprediction', 'CurrentTrends', 'Leaderboard', 'tshirt_neck_top_bottom.csv'))
         df = df[["Bigram", "Rating", "Count"]]
-
-        # order in the groupby here matters, it determines the json nesting the groupby call makes a pandas series by
-        # grouping 'the_parent' and 'the_child', while summing the numerical column 'child_size'
-        df1 = df.groupby(['Bigram', 'Rating'])['Count'].sum()
-        df1 = df1.reset_index()
-
-        # start a new flare.json document
-        flare = dict()
-        d = {"name": "flare", "children": []}
-
+        df1 = df.groupby(['Bigram', 'Rating'])['Count'].sum().reset_index()
+        flare = {'name': 'flare', 'children': []}
+        d = flare
         for line in df1.values:
-            Bigram = line[0]
-            Rating = line[1]
-            Count = line[2]
-
-            # make a list of keys
-            keys_list = []
-            for item in d['children']:
-                keys_list.append(item['name'])
-
-            # if 'the_parent' is NOT a key in the flare.json yet, append it
+            Bigram, Rating, Count = line
+            keys_list = [item['name'] for item in d['children']]
             if not Bigram in keys_list:
-                d['children'].append({"name": Bigram, "children": [{"name": Rating, "size": Count}]})
-
-            # if 'the_parent' IS a key in the flare.json, add a new child to it
+                d['children'].append({'name': Bigram, 'children': [{'name': Rating, 'size': Count}]})
             else:
-                d['children'][keys_list.index(Bigram)]['children'].append({"name": Rating, "size": Count})
-
-        flare = d
-        e = json.dumps(flare)
-        data.Prod2 = json.loads(e)
+                d['children'][keys_list.index(Bigram)]['children'].append({'name': Rating, 'size': Count})
+        data.Prod2 = json.loads(json.dumps(d))
         Prod2 = data.Prod2
 
-        df = pd.read_csv('CurrentTrends/Leaderboard/tshirt_print_top_bottom.csv')
-        # print(df.head())
+        df = pd.read_csv(os.path.join(BASE_DIR, 'trendprediction', 'CurrentTrends', 'Leaderboard', 'tshirt_print_top_bottom.csv'))
         df = df[["Bigram", "Rating", "Count"]]
-
-        # order in the groupby here matters, it determines the json nesting the groupby call makes a pandas series by
-        # grouping 'the_parent' and 'the_child', while summing the numerical column 'child_size'
-        df1 = df.groupby(['Bigram', 'Rating'])['Count'].sum()
-        df1 = df1.reset_index()
-
-        # start a new flare.json document
-        flare = dict()
-        d = {"name": "flare", "children": []}
-
+        df1 = df.groupby(['Bigram', 'Rating'])['Count'].sum().reset_index()
+        flare = {'name': 'flare', 'children': []}
+        d = flare
         for line in df1.values:
-            Bigram = line[0]
-            Rating = line[1]
-            Count = line[2]
-
-            # make a list of keys
-            keys_list = []
-            for item in d['children']:
-                keys_list.append(item['name'])
-
-            # if 'the_parent' is NOT a key in the flare.json yet, append it
+            Bigram, Rating, Count = line
+            keys_list = [item['name'] for item in d['children']]
             if not Bigram in keys_list:
-                d['children'].append({"name": Bigram, "children": [{"name": Rating, "size": Count}]})
-
-            # if 'the_parent' IS a key in the flare.json, add a new child to it
+                d['children'].append({'name': Bigram, 'children': [{'name': Rating, 'size': Count}]})
             else:
-                d['children'][keys_list.index(Bigram)]['children'].append({"name": Rating, "size": Count})
-
-        flare = d
-        e = json.dumps(flare)
-        data.Prod3 = json.loads(e)
+                d['children'][keys_list.index(Bigram)]['children'].append({'name': Rating, 'size': Count})
+        data.Prod3 = json.loads(json.dumps(d))
         Prod3 = data.Prod3
 
         context = {
@@ -333,4 +261,4 @@ def currenttrends(request):
             'bottom1_score': bottom1_score
         }
 
-        return render("website_currenttrends_tshirts.html", context)
+        return JsonResponse(context)
